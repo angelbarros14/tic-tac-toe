3b8be4da8c37e720403a13589806bcd9572650b9
angelbarros14 <social.angelbarros@gmail.com>
2025-04-03 05:56:39 +0800

    reset button

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 const game = (function () {
-    let gameboard = ['', '', '', '', '', '', '', '', ''];
+    let gameboard = ["", "", "", "", "", "", "", "", ""];
     let currentPlayer = "X";
     let gameStatus = true;
+
+
+    function reset() {
+        const cell = document.querySelectorAll(".cell") 
+        const resetButton = document.getElementById("reset")
+        resetButton.addEventListener("click", () => {
+            gameboard= ["", "", "", "", "", "", "", "", ""];
+            currentPlayer = "X";
+            gameStatus = true;
+            cell.forEach(cell => {
+                cell.textContent="";
+            })
+        })
+        
+    }
 
     function initialize() {
         const cell = document.querySelectorAll(".cell") 
         cell.forEach(cell => {
             cell.addEventListener("click", (event) => {
                 const index = parseInt(event.target.id);
+                // check if the cell is already clicked
+                if (gameboard[index] !== "") {
+                    return;
+                }
+
                 cell.textContent = currentPlayer;
-                console.log(event.target.id)
                 gameboard[index] = currentPlayer;
+                currentPlayer = currentPlayer == "X" ? "O" : "X"
+
+
+                console.log(event.target.id)
                 console.log(gameboard)
 
-            currentPlayer = currentPlayer == "X" ? "O" : "X"
 
-            // check if the cell is already clicked
             // check if all the cell is clicked
             // 
             })
@@ -23,6 +44,7 @@ const game = (function () {
     }
 
     initialize();
+    reset();
     return {
         
     };
