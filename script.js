fd248faecc94671f09e826a46029bd50298b9abc
angelbarros14 <social.angelbarros@gmail.com>
2025-04-03 23:50:59 +0800

    add winning indicator

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const game = (function () {
     let currentPlayer = "X";
     let gameStatus = true;
 
-
     function reset() {
         const cell = document.querySelectorAll(".cell") 
         const resetButton = document.getElementById("reset")
@@ -18,6 +17,8 @@ const game = (function () {
         
     }
 
+    reset();
+
     function initialize() {
         const cell = document.querySelectorAll(".cell") 
         cell.forEach(cell => {
@@ -25,37 +26,57 @@ const game = (function () {
                 const index = parseInt(event.target.id);
                 // check if the cell is already clicked
                 if (gameboard[index] !== "") {
+
                     return;
                 }
 
                 cell.textContent = currentPlayer;
                 gameboard[index] = currentPlayer;
                 currentPlayer = currentPlayer == "X" ? "O" : "X"
+                display();
 
+                if (win()) {
+                    const status = document.querySelector("p")
+                    status.textContent =  `Player ${currentPlayer} wins`
+                }
 
                 console.log(event.target.id)
                 console.log(gameboard)
 
-
-            // check if all the cell is clicked
-            // 
             })
         })
     }
 
-    initialize();
-    reset();
+    function display() {
+        const status = document.querySelector("p")
+        status.textContent = `Player ${currentPlayer}'s turn`;
+  
+    }
+
+    function win() {
+        const winningPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+
+        for (let pattern of winningPatterns) {
+            const [a, b, c] = pattern;
+            if (gameboard[a] == gameboard[b] && gameboard[a] == gameboard[c] && gameboard[a] != "") {
+                console.log("this is a win");
+                gameStatus = false;
+            }
+        }
+       return false;
+    }
+
+
     return {
-        
+        initialize,
+        reset,
+        win
     };
 
 })();
 
-
-
-// const cell = document.querySelectorAll(".cell")
-// cell.forEach(cell => {
-//     cell.addEventListener("click", () => {
-//         cell.textContent = cell.id;
-//     })
-// })
+game.initialize();
