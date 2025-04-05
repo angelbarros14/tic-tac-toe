cda008c093c7d5d714c46b8fe02c5dd972bffa57
angelbarros14 <social.angelbarros@gmail.com>
2025-04-05 23:43:14 +0800

    refactor

--- script.js
+++ script.js
@@ -25,24 +25,14 @@ const game = (function () {
         cells = document.querySelectorAll(".cell");
 
         cells.forEach(cell => {
-            // if cell is clicked
             cell.addEventListener("click", (event) => {
-                // find the index of the clicked cell
                 const index = parseInt(event.target.id);
-                // if the cell that's clicked is not an empty cell or if the game is over, ignore the click
-                if (gameboard[index] !== "" || !gameStatus  
-                ) {
-                    return;
-                }
+                if (gameboard[index] !== "" || !gameStatus) {return;}
                 
-        
-                // initial is X
                 cell.textContent = currentPlayer;
-                // if cell is clicked, switch player
                 toggle();
-                // add to gameboard array the current player
                 gameboard[index] = currentPlayer;
-                
+
                 draw();
                 win();  
                 reset();
@@ -75,14 +65,13 @@ const game = (function () {
 
         for (let pattern of winningPatterns) {
             const [a, b, c] = pattern;
-            // if pattern a is same as b and c AND if pattern in any is not ""
             if (gameboard[a] == gameboard[b] && gameboard[a] == gameboard[c] && gameboard[a] != "") {
                 pattern.forEach(cell => {
                     cells[cell].style.backgroundColor = "#E6FFE6";
                 })
+
                 display(`Player ${currentPlayer} wins`);
                 gameStatus = false;
-                return true;
             }
         }
     }
@@ -94,32 +83,3 @@ const game = (function () {
 })();
 
 game.initialize();
-
-/*
-refactor:
-function for checking winner
-function for checking draw
-function for switching player
-function for updating player status
-function for highlighting winning cells
-function for reset game
-function for handling cell clicks
-    if cell is filled
-    if game has winner
-    if game is draw
-    updating cell content
-    updating switching player
-function for initializing game
-*/
-
-/*
-when does display change?
-when a player switches or when cell is clicked
-when a player wins
-when game is draw
-when game is reset
-*/
-
-/*
-move dom references that will be populated
-*/
